8c314db63f9122c85c4a08bca95ed75e25b31acb
DumayaAI <dumayaaibusiness@gmail.com>
2026-09-20 02:37:04 +0800

    fix: use kotlin jvmToolchain(17) to atomically lock all plugins to JVM 17

--- mobile_app/android/build.gradle.kts
+++ mobile_app/android/build.gradle.kts
@@ -15,15 +15,11 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 
-    afterEvaluate {
-        tasks.withType<JavaCompile>().configureEach {
-            sourceCompatibility = "11"
-            targetCompatibility = "11"
-        }
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-            }
+    // Force ALL subprojects (plugins) to use the same JVM version.
+    // jvmToolchain locks both Java and Kotlin compilation atomically.
+    plugins.withId("org.jetbrains.kotlin.android") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension> {
+            jvmToolchain(17)
         }
     }
 }
